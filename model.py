55ffe501e5688dc36b8bbd8f7de958cb0defa418
iuhiyuh <58005994@qq.com>
2019-05-23 16:36:30 +0800

    Update model.py

--- model.py
+++ model.py
@@ -6,12 +6,10 @@ from torch import optim, nn
 class Model(nn.Module):
     def __init__(self, conf):
         super().__init__()
-
         self.num_layers = conf.num_layers
         self.hidden_dim = conf.hidden_dim
         self.class_num = conf.class_num
         self.device = conf.device
-
         self.lstm = nn.LSTM(input_size=conf.input_size
                             , hidden_size=self.hidden_dim
                             , num_layers=self.num_layers
@@ -21,14 +19,11 @@ class Model(nn.Module):
 
     def forward(self, input, hidden=None):
         batch_size, seq_len, _ = input.size()
-
         if hidden is None:
             h0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(self.device)
             c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(self.device)
         else:
             h0, c0 = hidden.to(self.device)
         output, hidden = self.lstm(input, (h0, c0))
-
         output = self.output(output[:, -1, :])
-
         return output
