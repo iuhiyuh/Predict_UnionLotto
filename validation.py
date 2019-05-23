7c1341b61d1f4d3768cfdb47ad5829b18ca83390
iuhiyuh <58005994@qq.com>
2019-05-23 16:37:34 +0800

    Update validation.py

--- validation.py
+++ validation.py
@@ -12,22 +12,17 @@ def val(datasets, model, idx, epoch, loss_logs, acc_logs, **kwargs):
     model.eval()
     for k, v in kwargs.items():
         setattr(conf, k, v)
-
     opt = optim.Adam(model.parameters())
     criterion = nn.CrossEntropyLoss(weight=conf.weight).to(conf.device)
-
     loss_log = []
     acc_log = []
     for step, (batch_x, batch_y) in enumerate(datasets[idx]):
         batch_x = batch_x.to(conf.device)
         batch_y = batch_y.to(conf.device)
-
         output = model(batch_x)
-
         loss = criterion(output, batch_y.long())
         los_log = loss.cpu().detach().numpy()
         loss_log.append(los_log)
-
         _, prediction = output.topk(1, 1, True)
         correct = prediction.t().eq(batch_y.view(1, -1).long())
         prediction = prediction.detach().cpu().numpy()
@@ -38,11 +33,9 @@ def val(datasets, model, idx, epoch, loss_logs, acc_logs, **kwargs):
               , '| Epoch: ', epoch + 1
               , '| validation loss: %.4f' % los_log
               , '| acc: %.4f' % n_correc_elems)
-
         if step == len(datasets[idx]) - 1:
             loss_logs.append(np.array(np.mean(loss_log)))
             acc_logs.append(np.array(np.mean(acc_log)))
-
         if (epoch + 1) % 100 == 0 and step == len(datasets[idx]) - 1:
             show_loss_acc(loss_logs, epoch + 1, "./val_result/{}号球".format(idx), "loss", "validation")
             show_loss_acc(acc_logs, epoch + 1, "./val_result/{}号球".format(idx), "acc", "validation")
