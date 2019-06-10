71465f30dbed48f65a4f90ae96499ea4e5484822
iuhiyuh <58005994@qq.com>
2019-06-10 20:40:20 +0800

    update for opt

--- train.py
+++ train.py
@@ -7,13 +7,11 @@ import numpy as np
 from show import show_loss_acc
 
 
-def train(datasets, model, idx, epoch, loss_logs, acc_logs, **kwargs):
+def train(opt, criterion, datasets, model, idx, epoch, loss_logs, acc_logs, **kwargs):
     conf = Config()
     model.train()
     for k, v in kwargs.items():
         setattr(conf, k, v)
-    opt = optim.Adam(model.parameters(), lr=conf.LR, weight_decay=conf.weight_decay)
-    criterion = nn.CrossEntropyLoss(weight=conf.weight).to(conf.device)
     loss_log = []
     acc_log = []
     for step, (batch_x, batch_y) in enumerate(datasets[idx]):
